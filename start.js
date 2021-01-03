3ec964b9a783660eb28f3b093df599407ac9543e
ALLCAPs788 <capspfifer@gmail.com>
2021-01-02 20:25:57 -0500

    template

--- start.js
+++ start.js
@@ -5,3 +5,14 @@ remember filter statements from previous assignments
 intro javascript d3: for each statements, 
 "metadata" is a part of the json
 ask Jacob/Peter about starter code for javascript 
+
+//horizontal bargraph template
+*/
+var data = [{
+  type: 'bar',
+  x: [20, 14, 23],
+  y: ['giraffes', 'orangutans', 'monkeys'],
+  orientation: 'h'
+}];
+
+Plotly.newPlot('myDiv', data);
