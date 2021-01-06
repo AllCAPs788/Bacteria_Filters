503d3421a3588660c10a012ba3588ada32ed63dc
ALLCAPs788 <capspfifer@gmail.com>
2021-01-06 18:35:23 -0500

    fixed filter

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,4 @@
-/* take json, turn into traces for plotly
-use dropdowns
-horizontal, bubble, gauge optional
-remember filter statements from previous assignments
-intro javascript d3: for each statements, 
-"metadata" is a part of the json
-ask Jacob/Peter about starter code for javascript 
-
-//horizontal bargraph template
-*/
-
-/*1. Use the D3 library to read in samples.json.
-
-
-Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-
-
-
+/*Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 Use sample_values as the values for the bar chart.
 
 
@@ -36,89 +18,91 @@ Use otu_labels for the text values.
 
 //d3.json, load .then
 var url = '../../samples.json';
-d3.json(url).then(function(samples) {
-  console.log(samples);
-});
+// d3.json(url).then(function(samples) {
+//   console.log(samples);
+// });
 //init populate dropdowns, dropdown change, filtering
 
+var dropdown = d3.select("#selDataset");
 
-  
+// //create dropdowns via javascript
+// var dataset = dropdown.property("value");
 
-var dataset = dropdown.property("value");
-var x = [];
-var y = [];
+d3.selectAll("#selDataset").on("change", optionChanged);
 
-var data = [{
-  type: 'bar',
-  
-  x: [0,50,100,150],
-  y: [dataset];
-  orientation: 'h'
-  layout = ];
-
+function optionChanged(value) {
+  //var name = dropdown.property("value");
+  d3.json(url).then(function(data) {
+  //filter and map statements 
+  //filter value variable for specific information and correct json: either names, metadata, or samples, refer to GitLab
+  //add text to demo table (id="sample-metadata"), add h tags for text 
+  //refer to formatted json
+  var samples = data.samples;
+  var sorted_sample_values = samples.filter(sample => sample.id == value);
+  console.log(sorted_sample_values);
+  //add plotly traces (or make sure plotly can see these defined statements for json and filtering)
+  });
+};
 
 
 
 //bubble template
-var trace1 = {
-  x: [1, 2, 3, 4],
-  y: [0,50,100,150,200],
-  mode: 'markers',
-  marker: {
-    size: [0,500,1000,1500,2000,2500,3000]
-  }
-};
-var layout = {
-  hovermode: 'closest',
-  title:'Hover on a Point<br>to Change Color'
-};
-var dropdown = d3.select("#selDataset")
-d3.selectAll("#selDataset").on("change", editgraph);
+// var trace1 = {
+//   x: [1, 2, 3, 4],
+//   y: [0,50,100,150,200],
+//   mode: 'markers',
+//   marker: {
+//     size: [0,500,1000,1500,2000,2500,3000]
+//   }
+// };
+// var layout = {
+//   hovermode: 'closest',
+//   title:'Hover on a Point<br>to Change Color'
+// };
 
-function editgraph() {
-  var name = dropdown.property("value");
-};
 
 //needs data object
-  var data = [trace1];
+//   var data = [trace1];
   
-  var layout = {
-    title: 'Marker Size',
-    showlegend: false,
-    height: 600,
-    width: 600
-  var bacteria_trace = {  
-    x:
-    y:
-    text: //filter statement for bacteria id
-    name: "Top 10 Bacteria Found"
-    type: "bar"
-    orientation: 'h'
-};
+//   var layout = {
+//     title: 'Marker Size',
+//     showlegend: false,
+//     height: 600,
+//     width: 600
+//   var bacteria_trace = {  
+//     x:
+//     y:
+//     text: //filter statement for bacteria id
+//     name: "Top 10 Bacteria Found"
+//     type: "bar"
+//     orientation: 'h'
+// };
   
 
-Plotly.newPlot('myDiv', data, layout);
-var data = [trace1];
-var layout = {
-    title: "Top 10 Bacteria Found",
-    // fill in margin values
-    margin: {
-      l: 
-      r:
-      t:
-      b:
-      hovermode:'closest',
+// Plotly.newPlot('myDiv', data, layout);
+// var data = [trace1];
+// var layout = {
+//     title: "Top 10 Bacteria Found",
+//     // fill in margin values
+//     margin: {
+//       l: 
+//       r:
+//       t:
+//       b:
+//       hovermode:'closest',
       
-    }
-  };
-  Plotly.newPlot('bar', data, layout);
+//     }
+//   };
+  //Plotly.newPlot('bar', data, layout);
   // import json, define sample ids and get dropdown values
   function init() {
-  var url = '../../samples.json';
+  //var url = '../../samples.json';
   d3.json(url).then(function(samples) {
     //console.log(samples);
-    var sample_id = samples.names 
+    var sample_id = samples.names;
     sample_id.forEach(names => { 
       dropdown.append("option").text(names).property("value", names);
     });
   }); 
+};
+  init();
